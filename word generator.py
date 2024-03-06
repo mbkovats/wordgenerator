113f8b268607f15578791b5a64f438963493a47c
Marcell_Kovats <mbkovats@buffalo.edu>
2024-03-05 21:31:22 -0500

    comments

--- word generator.py
+++ word generator.py
@@ -21,53 +21,70 @@ while(nulls == "" or int(nulls) < 1):
     if int(nulls) <= 0:
         print("The number of word terminators cannot be negative or zero.")
 
-while(words == "" or int(words) < 1):
-    words = input("How many words would you like to generate? ")
-
+# Add the word terminators to the list of letters
 for i in range(int(nulls)):
     letters.append("NULL")
 
+while(words == "" or int(words) < 1):
+    words = input("How many words would you like to generate? ")
+
+
 for i in range(int(words)):
     word = ""
+    # Keep track of previous letter
     prev = "NULL"
+    # Create a word
     while(True):
         choice = random.choice(letters)
+        # Count the number of vowels in the word
         vowcount = 0
         if choice in vowels:
             vowcount += 1
         if len(word) > int(maxlen):
             print(word)
             break
+        # If you hit a word terminator, print the word after doing some checks to see if it is valid
         if choice == "NULL":
+            # Check if the word has at least one vowel and one consonant and is longer than the minimum length
             if any([letter in word for letter in vowels]) and any([letter in word for letter in consonants]) and len(word) > int(minlen):
+                # Words can't end with J or V
                 if prev == "J" or prev == "V":
                     word += "E"
-                if prev == "F" or prev == "L" or prev == "S" and vowcount == 1 and word[-2] != prev:
+                # Floss Rule
+                if prev in ["F", "L", "S", "Z"] and vowcount == 1 and word[-2] != prev:
                     word += prev
                 prevletter = "NULL"
                 prevprevletter = "NULL"
                 for j,letter in enumerate(word):
                     if prevletter != "NULL" and prevprevletter != "NULL":
+                        # Make consonants double if vowel before is short and there are 2 syllables. Example: rabbit or letter
                         if vowcount == 2:
                             if prevprevletter in consonants and prevletter in vowels and letter in consonants:
                                 word = word[:j] + letter + word[j:]
+                        # Make sure there are no triple letters
                         if prevletter == prevprevletter and prevletter == letter:
                             word = word[:j-1] + word[j:]
                     prevprevletter = prevletter
                     prevletter = letter
                 print(word)
                 break
+            # If the word doesn't meet the requirements, keep adding letters until it does
             else:
                 pass
+        # If it's not a word terminator
         else:
+            # Q is always followed by U
             if choice == "Q":
                 word += "QU"
                 vowcount += 1
                 prev = "U"
+            # If there are back to back consonants, make sure they are valid
             elif prev in consonants and choice in consonants:
+                # Two of the same consonant in a row is allowed
                 if prev == choice and len(word) > 2 and word[-2] != choice:
                     word += choice
                     prev = choice
+                # Specific consonant blends
                 elif prev == "D":
                     if choice == "R":
                         word += choice
@@ -110,7 +127,9 @@ for i in range(int(words)):
                         prev = choice
                 else:
                     pass
+            # Back to back vowels are always allowed
             elif prev in vowels and choice in vowels:
+                # I before E except after C
                 if (prev == "I" or choice == "I") and (prev == "E" or choice == "E"):
                     if len(word) > 2 and word[-2] == "C":
                         word = word[:-1] + "E"
@@ -120,12 +139,16 @@ for i in range(int(words)):
                         word = word[:-1] + "I"
                         word += "E"
                         prev = "E"
+            # Consonants are always allowed after vowels or vice versa
             else:
+                # C is always K if followed by E, I, or Y
                 if prev == "C":
                     if choice in ["E","I","Y"]:
                         word = word[:-1] + "K" + choice
                     else:
                         word += choice
+                # Add the letter to the word if it's not a special case
                 else:
                     word += choice
+                # Update the previous letter
                 prev = choice
