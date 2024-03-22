41b36e2371f5930feb7a873d08e64e84cecdbbce
Marcell_Kovats <mbkovats@buffalo.edu>
2024-03-22 06:35:32 -0400

    fixed if word is all vowels and consonants bug

--- word generator.py
+++ word generator.py
@@ -1,13 +1,13 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 vowels = ["A", "E", "I", "O", "U"]
-consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
 import random
 
 minlen = ""
 maxlen = ""
 nulls = ""
 words = ""
-
+# Prompt the user for the minimum and maximum word length
 while(minlen == "" or maxlen == "" or int(minlen) > int(maxlen) or int(minlen) < 3):
     minlen = input("Minimum word length: ")
     maxlen = input("Maximum word length: ")
@@ -18,6 +18,7 @@ while(minlen == "" or maxlen == "" or int(minlen) > int(maxlen) or int(minlen) <
     elif int(minlen) < 3:
         print("The minimum word length cannot be 2 or less.")
 
+# Prompt the user for the number of word terminators
 while(nulls == "" or int(nulls) < 1):
     nulls = input("How many word terminators would you like? (The larger the number, the smaller the average word length): ")
     if nulls == "":
@@ -29,11 +30,17 @@ while(nulls == "" or int(nulls) < 1):
 for i in range(int(nulls)):
     letters.append("NULL")
 
+# Prompt the user for the number of words to generate
 while(words == "" or int(words) < 1):
     words = input("How many words would you like to generate? ")
 
-
+# Generate the words
 for i in range(int(words)):
+    # Decide if Y is a vowel or consonant
+    if random.randint(0,1) == 1:
+        vowels.append("Y")
+    else:
+        consonants.append("Y")
     word = ""
     # Keep track of previous letter
     prev = "NULL"
@@ -46,32 +53,37 @@ for i in range(int(words)):
             vowcount += 1
         # If you hit a word terminator or the length of the word is greater than the maximum length, print the word after doing some checks to see if it is valid
         if choice == "NULL" or len(word) >= int(maxlen):
-            # Check if the word has at least one vowel and one consonant and is longer than the minimum length
-            if any([letter in word for letter in vowels]) and any([letter in word for letter in consonants]) and len(word) >= int(minlen):
-                # Words can't end with J or V
-                if prev == "J" or prev == "V":
-                    word += "E"
-                # Floss Rule
-                if prev in ["F", "L", "S", "Z"] and vowcount == 1 and word[-2] != prev:
-                    word += prev
-                prevletter = "NULL"
-                prevprevletter = "NULL"
-                for j,letter in enumerate(word):
-                    if prevletter != "NULL" and prevprevletter != "NULL":
-                        # Make consonants double if vowel before is short and there are 2 syllables. Rabbit rule.
-                        if vowcount == 2 and j < len(word) - 1:
-                            if prevprevletter in consonants and prevletter in vowels and letter in consonants and word[-1] not in vowels and word[j+1] in vowels and letter in ["B","D","R","P","T","M"] and random.randint(0,1) == 1:
-                                word = word[:j] + letter + word[j:]
-                        # Make sure there are no triple letters
-                        if prevletter == prevprevletter and prevletter == letter:
-                            word = word[:j-1] + word[j:]
-                    prevprevletter = prevletter
-                    prevletter = letter
-                print(word)
-                break
-            # If the word doesn't meet the requirements, keep adding letters until it does
-            else:
-                pass
+            # Check if the word is at least the minimum length, otherwise keep going
+            if len(word) >= int(minlen):
+                # Check if the word has at least one vowel and one consonant
+                if vowcount > 0 and any([letter in word for letter in consonants]):
+                    # Words can't end with J or V
+                    if prev == "J" or prev == "V":
+                        word += "E"
+                    # Floss Rule
+                    if prev in ["F", "L", "S", "Z"] and vowcount == 1 and word[-2] != prev:
+                        word += prev
+                    prevletter = "NULL"
+                    prevprevletter = "NULL"
+                    for j,letter in enumerate(word):
+                        if prevletter != "NULL" and prevprevletter != "NULL":
+                            # Make consonants double if vowel before is short and there are 2 syllables. Rabbit rule.
+                            if vowcount == 2 and j < len(word) - 1:
+                                if prevprevletter in consonants and prevletter in vowels and letter in consonants and word[-1] not in vowels and word[j+1] in vowels and letter in ["B","D","R","P","T","M"] and random.randint(0,1) == 1:
+                                    word = word[:j] + letter + word[j:]
+                            # Make sure there are no triple letters
+                            if prevletter == prevprevletter and prevletter == letter:
+                                word = word[:j-1] + word[j:]
+                        prevprevletter = prevletter
+                        prevletter = letter
+                    print(word)
+                    break
+
+                # If the word is all vowels or consonants, start over
+                elif vowcount == len(word) or vowcount == 0:
+                    word = ""
+                    vowcount = 0
+                    prev = "NULL"
         # If it's not a word terminator
         else:
             # Q is always followed by U
@@ -79,7 +91,7 @@ for i in range(int(words)):
                 word += "QU"
                 vowcount += 1
                 prev = "U"
-            # If there are back to back consonants, make sure they are valid
+            # If there are back to back consonants in the word, make sure they are valid
             elif prev in consonants and choice in consonants:
                 # Two of the same consonant in a row is allowed
                 if prev == choice and len(word) > 2 and word[-2] != choice:
@@ -160,3 +172,9 @@ for i in range(int(words)):
                     word += choice
                 # Update the previous letter
                 prev = choice
+    
+    # Remove Y from the list of vowels or consonants
+    if "Y" in vowels:
+        vowels.remove("Y")
+    else:
+        consonants.remove("Y")
